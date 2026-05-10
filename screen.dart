02f72e2e5dccd20ec79755b410d01237ea996206
Ayman2023bo <152474545+Ayman2023bo@users.noreply.github.com>
2026-05-10 12:39:22 +0300

    Add initial Screen widget

---
+++ screen.dart
@@ -0,0 +1,13 @@
+class Screen extends StatefulWidget {
+  const Screen({super.key});
+
+  @override
+  State<Screen> createState() => _ScreenState();
+}
+
+class _ScreenState extends State<Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
