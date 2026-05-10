92a6c7ca14dacb3c948ac55cc5310c35cc3a36e6
Ayman2023bo <152474545+Ayman2023bo@users.noreply.github.com>
2026-05-10 13:06:58 +0300

    Create Wadgit

---
+++ lib/wadgits.dart
@@ -0,0 +1,13 @@
+class Wadgit extends StatefulWidget {
+  const Screen({super.key});
+
+  @override
+  State<Screen> createState() => _ScreenState();
+}
+
+class Wadgit extends State<Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
