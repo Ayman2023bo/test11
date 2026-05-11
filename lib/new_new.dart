7b009cb09ac716f1abb4a18a75221c11612f46b0
ayman <aymanalbonny@gmail.com>
2026-05-11 15:50:15 +0300

    uplod  branch ayman

---
+++ lib/new_new.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class New_new extends StatelessWidget {
+  const New_new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
